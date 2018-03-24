dee6ea0ef2dc02d641e0b566ef507b89b6abb696
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-25 03:20:06 +0530

    Updated

--- src/main/java/com/nagarro/authenticationservice/App.java
+++ src/main/java/com/nagarro/authenticationservice/App.java
@@ -2,11 +2,13 @@ package com.nagarro.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class App 
 {
