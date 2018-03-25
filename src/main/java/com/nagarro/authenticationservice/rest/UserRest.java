42f6d18398285a4c63a974f1204aaa58dfa69b2e
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-25 19:55:00 +0530

    Updated

--- src/main/java/com/nagarro/authenticationservice/rest/UserRest.java
+++ src/main/java/com/nagarro/authenticationservice/rest/UserRest.java
@@ -3,10 +3,6 @@ package com.nagarro.authenticationservice.rest;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
