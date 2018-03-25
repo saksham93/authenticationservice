bba6ee8fe72c02ea0509103fad95e737acb90207
NAGARRO\sakshamgupta <saksham.gupta@nagarro.com>
2018-03-25 19:06:03 +0530

    Remove discovery

--- src/main/java/com/nagarro/authenticationservice/App.java
+++ src/main/java/com/nagarro/authenticationservice/App.java
@@ -2,13 +2,12 @@ package com.nagarro.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Hello world!
  *
  */
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class App 
 {
